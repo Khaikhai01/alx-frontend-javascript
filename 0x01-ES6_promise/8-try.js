864a5a66f68b01d31e4b6ebb53a5aa7a3651fb5d
purity <shishchei@gmail.com>
2023-06-28 18:02:20 +0000

    'code fail fixed'

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,11 @@
 export default function divideFunction(numerator, denominator) {
   try {
+    const divide = numerator / denominator;
     if (denominator === 0) {
-    throw new Error();
-  }
-
-  return numerator / denominator;
-  } catch(error) {
-      throw new Error('Cannot divide by 0');
+      throw new Error();
+    }
+    return divide;
+  } catch (err) {
+    throw Error('cannot divide by 0');
   }
 }
