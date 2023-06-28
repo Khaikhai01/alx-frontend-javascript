90a086a2ca4a8e5de4fe2aefce39e54da4f2918f
purity <shishchei@gmail.com>
2023-06-28 11:13:50 +0000

    'eslint fix'

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,11 +5,11 @@ export default function handleProfileSignup() {
   const data2 = createUser();
   return Promise.all([data1, data2])
     .then((results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);      
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch((err) => {
       if (err) {
         console.log('Signup system offline');
       }
-    })
+    });
 }
