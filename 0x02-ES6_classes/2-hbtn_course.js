44a3da39ad4711b75b07f769f9471010361e1519
purity <shishchei@gmail.com>
2023-06-29 20:13:52 +0000

    eslint fix and  Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,47 +3,53 @@ export default class HolbertonCourse {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw new TypeError("Name must be a string")
+      throw new TypeError('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
     } else {
-      throw new TypeError("Length must be a number")
+      throw new TypeError('Length must be a number');
     }
     if (Array.isArray(students)) {
       this._students = students;
     } else {
-      throw new TypeError("Students must be an array")
+      throw new TypeError('Students must be an array');
     }
   }
+
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw new TypeError("Name must be a string")
+      throw new TypeError('Name must be a string');
     }
   }
+
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
     } else {
-      throw new TypeError("Length must be a number")
+      throw new TypeError('Length must be a number');
     }
   }
+
   set students(students) {
     if (Array.isArray(students)) {
       this._students = students;
     } else {
-      throw new TypeError("Students must be an array")
+      throw new TypeError('Students must be an array');
     }
   }
 }
